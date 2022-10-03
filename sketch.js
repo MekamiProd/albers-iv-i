0420a94157e4b28ccd332940d53ae8ef11388654
MekamiProd <85590407+MekamiProd@users.noreply.github.com>
2022-10-02 23:36:04 -0500

    color3
    chose more pastel-esque colors

--- sketch.js
+++ sketch.js
@@ -1,36 +1,37 @@
 
 let clrs = [
-'#6600FF', //top 0
-'#DDF0C0', //middle top 1
-'#00FF7F', //middle strip 2
-'#120A8F', //middle bottom 3
-'#F3E5AB' //bottom 4
-]
-
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noStroke();
-}
-
-function draw() {
- background(220);
- canvasRect(clrs[4], height/2, height/2, false);
- canvasRect(clrs[0], height/2, 0, false);
- fill(clrs[2]); 
- rect(width * 1/3, height * 1/6, width * 1/3, height * 2/3); 
- canvasRect(clrs[1], height/6, height/3, true);
- canvasRect(clrs[3], height/6, height/2, true);
-
-
-}
-
-function canvasRect(clr, hgt, y_offset, clickable) {
-	fill(clr);
-	if((clickable == true) && (mouseIsPressed == false)){
-		rect(0, y_offset, width, hgt);
-	} 
-	if(clickable == false){
-		rect(0, y_offset, width, hgt);
-	}
-}
+  '#560139', //top 0
+  '#DE3163', //middle top 1
+  '#FFF5EE', //middle strip 2
+  '#ABCDEF', //middle bottom 3
+  '#ACE1AF' //bottom 4
+  ];
+  
+  
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+  }
+  
+  function draw() {
+   background(220);
+   canvasRect(clrs[4], height/2, height/2, false);
+   canvasRect(clrs[0], height/2, 0, false);
+   fill(clrs[2]); 
+   rect(width * 1/3, height * 1/6, width * 1/3, height * 2/3); 
+   canvasRect(clrs[1], height/6, height/3, true);
+   canvasRect(clrs[3], height/6, height/2, true);
+  
+  
+  }
+  
+  function canvasRect(clr, hgt, y_offset, clickable) {
+    fill(clr);
+    if((clickable == true) && (mouseIsPressed == false)){
+      rect(0, y_offset, width, hgt);
+    } 
+    if(clickable == false){
+      rect(0, y_offset, width, hgt);
+    }
+  }
+  
